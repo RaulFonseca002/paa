13716aae577f3ed2733ef747985943b498f5fa25
Wallace Oliveira <wallaceoliv05@gmail.com>
2024-08-16 14:38:57 -0300

    log class start

---
+++ paa02/Log.cpp
@@ -0,0 +1,31 @@
+
+#include <string.h>
+#include <iostream>
+
+using namespace std;
+
+class Log {
+	public void init(string key) {
+		
+	}
+
+	public void end(string key) {
+
+	}
+
+	public void incrementInstance(string key) {
+
+	}
+
+	public void incrementAttribuition(string key) {
+
+	}
+
+	public void incrementCompare(string key) {
+
+	}
+
+	public void printLog(string id) {
+
+	}
+};
